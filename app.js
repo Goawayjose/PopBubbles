fae1d17b7c399c826071c2bb5f6dd9dbf10497ad
Jose Ramirez Imac <Jrramirez1994@gmail.com>
2017-04-11 03:04:35 -0700

    Music Player and dislike feature progress
    music player needs skip function glitch fixed, dislike could use
    additional animations and affordances. I also found ways to solve
    Jquery issues with Vue and SASS

--- app.js
+++ app.js
@@ -15,8 +15,10 @@ var app = new Vue({
       //getting top-100
       axios.get('songs.php')
         .then(function (response){
+
+
           app.songs = response.data;
-          // console.log(app.songs.length);
+
 
           for (i = 0; i < app.songs.length; i++) {
 
@@ -33,6 +35,7 @@ var app = new Vue({
                     app.tracks.push(response.tracks.items[1]);
                       // console.log(items);
 
+
                   }
               });
           }
@@ -44,20 +47,110 @@ var app = new Vue({
     methods: {
 
       playEt: function (name) {
-
+          app.onPlay.splice(0,app.onPlay.length);
           app.onPlay.push(name);
+          var daId = app.onPlay[0].id
+          $('#' + daId + '').addClass('playing');
+      },
+
+      playOrPause: function (){
+      var mytrack = document.getElementById('myTrack');
+      var duration = document.getElementById('fullDuration');
+      var currentTime = document.getElementById('currentTime');
+
+      var barSize = 250;
+      var bar = document.getElementById('defualtBar');
+      var progressbar = document.getElementById('progressBar');
+
+
+      var minutes = parseInt(mytrack.duration/60);
+      var seconds = parseInt(mytrack.duration%60);
+
+      bar.addEventListener('click', clickedBar, false);
+
+      duration.innerHTML = minutes + ':' + seconds;
+
+      if(!mytrack.paused && !mytrack.ended){
+        mytrack.pause();
+        $('.play').css('display', 'block');
+        $('.pause').css('display', 'none');
+        window.clearInterval(updateTime);
       }
+      else {
+        mytrack.play();
+        $('.play').css('display', 'none');
+        $('.pause').css('display', 'block');
+        updateTime = setInterval(update, 500);
+      }
+
+      function pad(d) {
+        return (d < 10) ? '0' + d.toString() : d.toString();
+      }
+
+      function update(){
+        if(!mytrack.ended) {
+            var playedMinutes = parseInt(mytrack.currentTime/60);
+            var playedSeconds =pad(parseInt(mytrack.currentTime%60));
+            currentTime.innerHTML = playedMinutes + ':' + playedSeconds;
+
+            var size = parseInt(mytrack.currentTime*barSize/mytrack.duration);
+            progressBar.style.width = size + "px";
+        }
+        else{
+          currentTime.innerHTML = "0:00";
+          $('.play').css('display', 'block');
+          $('.pause').css('display', 'none');
+          progressBar.style.width = "0px";
+          window.clearInterval(updateTime);
+        }
+      }
+
+      function clickedBar(e) {
+        if (!mytrack.ended) {
+          var mouseX = e.pageX - bar.offsetLeft;
+          var newtime = mouseX * mytrack.duration/barSize;
+
+          mytrack.currentTime = newtime;
+          progressBar.style.width = mouseX + "px";
+        }
+      }
+
+      var volumeSlider = document.getElementById('volumeSlider');
+
+      volumeSlider.addEventListener('change', function() {
+        console.log('work');
+        mytrack.volume = volumeSlider.value / 100;
+      });
+    },
+
+    shareDis: function (){
+      alert('me');
+    },
+
+    disLike: function (){
+      var daId = app.onPlay[0].id
+      $('#' + daId + '').addClass('removing');
+      app.onPlay.splice(0,app.onPlay.length);
+    }
+
 
     }
 
 
 
+
 });
 
 
-var bubble = $('.bubble');
+var mytrack = document.getElementById('myTrack');
 
 
-bubble.click(function() {
-  alert('me');
-});
+
+
+/* Questions:
+
+- getting jquery to work after bubbles are loaded?
+- why is the audio skipper not working?
+- going about getting top-100 for different genres?
+
+*/
