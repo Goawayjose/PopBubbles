8b5c596382b7e3f0b1b29c55a4cdc1e4b498dd75
Jose Ramirez Imac <Jrramirez1994@gmail.com>
2017-05-02 00:37:03 -0700

    working online

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>spotify</title>
+    <title>Pop Bubbles</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css?family=Lato:300,400" rel="stylesheet">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
